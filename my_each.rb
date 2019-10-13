5a69b4c2f4f4b41576c9d2bfe82a0dc27f0f9d8e
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-13 20:26:57 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,6 +4,7 @@ def my_each(array)
   while i < array.length
   # while i is smaller than the size of the array because we only need the number of elements in the array
     yield(array[i])
+    #array[i] are the elements being run on the block that we yield the control to manipulate the data
     i+=1
   end
   array

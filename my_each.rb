6722bca14b06d883dd0f37dc883cfffeb8efa1de
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-13 20:23:46 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,7 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+  #counter starts in 0 and is used as the index integer to acces the elements in the array
+  while i < array.length
+  # while i is smaller than the size of the array because we only need the number of elements in the array
+  
 end

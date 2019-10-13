49ab285799fa23f21c7c86fc52ea4966ad6d0401
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-13 20:24:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,5 +3,8 @@ def my_each(array)
   #counter starts in 0 and is used as the index integer to acces the elements in the array
   while i < array.length
   # while i is smaller than the size of the array because we only need the number of elements in the array
-  
+    yield(array[i])
+    i+=1
+  end
+  array
 end
